7796c09cd255cf5b4c9257fef37115bcc7b99eb4
KhattiyaTN <KhattiyaDCI072@gmail.com>
2025-02-23 17:28:10 +0700

    Setup theme's project structure and Colors
    - Add css to vite.config.js
    - Add color theme to tailwind.config.js
    - Add layer app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-/** @type {import('tailwindcss').Config} */
+/**ช @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -14,6 +14,69 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        colors: {
+            primary: {
+                light: '#821131',
+                DEFAULT: '#821131',
+                dark: '#821131',
+            },
+            secondary: {
+                light: '#FBA01D',
+                DEFAULT: '#FBA01D',
+                dark: '#FBA01D',
+            },
+            card: {
+                light: '#D9D9D9',
+                DEFAULT: '#D9D9D9',
+                dark: '#D9D9D9',
+            },
+            hover: {
+                light: '#808080',
+                DEFAULT: '#808080',
+                dark: '#808080',
+            },
+            /* ========================
+                        text 
+            =========================*/
+            headerText: {
+                light: '#B8B20A',
+                DEFAULT: '#B8B20A',
+                dark: '#B8B20A',
+            },
+            bodyText: {
+                light: '#FFFFFF',
+                DEFAULT: '#FFFFFF',
+                dark: '#FFFFFF',
+            },
+            /* ========================
+                        Button 
+            =========================*/
+            primaryButton: {
+                light: '#821131',
+                DEFAULT: '#821131',
+                dark: '#821131',
+            },
+            secondaryButton: {
+                light: '#118267',
+                DEFAULT: '#118267',
+                dark: '#118267',
+            },
+            infoButton: {
+                light: '#53A9F7',
+                DEFAULT: '#53A9F7',
+                dark: '#53A9F7',
+            },
+            warningButton: {
+                light: '#F4C312',
+                DEFAULT: '#F4C312',
+                dark: '#F4C312',
+            },
+            dangerButton: {
+                light: '#FF0F00',
+                DEFAULT: '#FF0F00',
+                dark: '#FF0F00',
             },
         },
     },
